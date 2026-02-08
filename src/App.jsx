ee5187c691510d50bc8e32a4c6395bef4a3abdce
Surwxa5 <nepalisurakshya767@gmail.com>
2026-02-08 13:06:33 +0545

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
-import logo from '/'
+import logo from "../Images/netflixlogo.jpg";
+import bgImage from "../Images/BackgroundImage.jpg"
 
 const Navbar = () => {
   return (
     <div>
       <div>
-    <img src={logo} alt="image"/>
+        <div className='h-screen bg-cover bg-center' style={{ backgroundImage: `url(${bgImage}` }} >
+          <div>
+            <img className='w-40' src={logo} alt="image" />
+            <button className='bg-red-600 text-white border-'>singin</button>
+          </div>
+          {/* heading section */}
+          <div className=' text-center text-white '>
+            <h1 className='font-bold  text-7xl '>Unlimited movies,TV  <br />shows, and more</h1>
+            <h2 className='font-semibold text-3xl mt-10'>Starts at USD 2.99. Cancel anytime.</h2>
+
+            <p className='mt-10 text-2xl'>Ready to watch? Enter your email to create or restart your membership.</p>
+          </div>
+          {/* input of hero section */}
+          <div>
+            <input type="text" placeholder='Email Address ' />
+            <button>Get Started <span>></span> </button>
+          </div>
+          <div>
+
+          </div>
+        </div>
       </div>
     </div>
   )
