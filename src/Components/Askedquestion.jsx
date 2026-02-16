6b3f8e13e8e75520435fa0009209367f55ae6044
Surwxa5 <nepalisurakshya767@gmail.com>
2026-02-16 20:13:14 +0545

    asked question

--- src/Components/Askedquestion.jsx
+++ src/Components/Askedquestion.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 
 const Askedquestion = () => {
+  const FAQData =[
+    {
+      title:"What is Netflix",
+    description:"Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are "
+    },
+    {
+      title:"What is Netflix",
+    description:"Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are "
+    }
+  ]
   return (
     <div>
       <h1 className='mt-20 text-3xl'>Frequently Asked Questions</h1>
